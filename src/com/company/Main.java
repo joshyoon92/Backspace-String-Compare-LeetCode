c35b096309444eeb4d12a25117b92616c21bb208
Joshua Yoon <joshyoon92@gmail.com>
2021-03-27 08:54:58 +0900

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+    public String build(String S){
+        Stack<Character> stack = new Stack<Character>();
+        for (char c: S.toCharArray()) {
+            if (c != '#'){
+                stack.push(c);
+            }
+            else if (!stack.empty()) {
+                stack.pop();
+            }
+        }
+        return String.valueOf(stack);
+    }
+}
